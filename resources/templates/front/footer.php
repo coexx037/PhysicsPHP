397e142c0ba173a6260458c52d8448a9c07d1351
David Coe <coexx037@gmail.com>
2017-07-04 23:29:45 +0000

    change footer

--- resources/templates/front/footer.php
+++ resources/templates/front/footer.php
@@ -6,7 +6,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2030</p>
+                    <p>2017 David Coe</p>
                 </div>
             </div>
         </footer>
