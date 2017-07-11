23a6ac60308ac3a5cb8c17a1e49efc225e1031aa
David Coe <coexx037@gmail.com>
2017-07-11 02:48:24 +0000

    gval

--- public/js/methods.js
+++ public/js/methods.js
@@ -26,7 +26,7 @@ $.each(options, function(key, value) {
     
     var gval = (key == "g") ? 9.81 : '';    
 
-	var textInput = '<td><input type="text" id='+key+' name="vall['+key+']" value="'+gval+'"></td>'
+	var textInput = '<td><input type="text" id='+key+' name="vall['+key+']" value='+gval+'></td>'
 	var unitsSelect = '<td><select id='+key+' name="units['+key+']"></select></td>'
 	
 	$('#solve_data').append('<option value='+key+'>'+key+'</option>')
