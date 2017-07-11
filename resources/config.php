38ed38b86796cbf57bee2b1a5fcf33049e613c34
David Coe <coexx037@gmail.com>
2017-07-11 04:30:04 +0000

    new database

--- resources/config.php
+++ resources/config.php
@@ -12,12 +12,11 @@ define("TEMPLATE_FRONT", __DIR__.DS."templates/front");
 define("TEMPLATE_BACK", __DIR__.DS."templates/back");
 
 define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'); /*Database server*/
-define('DB_NAME', 'heroku_22507e1fad7879c'); /*Database Name*/
-define('DB_USER', 'bfe6b7c7f23470'); /*Database username*/
-define('DB_PWD', 'ad54ba23');
+define('DB_NAME', 'heroku_3cfc984b56386d0'); /*Database Name*/
+define('DB_USER', 'b49eab265c3201'); /*Database username*/
+define('DB_PWD', '	cc88e286');
 
-//mysql://bfe6b7c7f23470:ad54ba23@us-cdbr-iron-east-03.cleardb.net/heroku_22507e1fad7879c?reconnect=true
-
+//mysql://b49eab265c3201:cc88e286@us-cdbr-iron-east-03.cleardb.net/heroku_3cfc984b56386d0?reconnect=true
 
 function connectDB() {
     $link = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
