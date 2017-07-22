3675cfd6724da4f71952ffd633742e430851555d
David Coe <coexx037@gmail.com>
2017-07-22 23:01:31 +0000

    me

--- resources/templates/front/top_nav.php
+++ resources/templates/front/top_nav.php
@@ -4,20 +4,22 @@
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <a class="navbar-brand" href="index.php">Home</a>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <!--<div id="bs-example-navbar-collapse-1">-->
                     
-                    <?php
-                    
-                        if(!isset($_SESSION['user_id'])){
-                            echo "<a class='nav navbar-nav navbar-right' href='Login.php'>Login</a>";
-                            echo "<a class='nav navbar-nav navbar-right' href='AddUser.php'>Sign Up</a>";
-                        }else{
-                            echo "<a class='nav navbar-nav navbar-right' href='Logout.php'>Logout</a>";
-                        }
-                    
-                    ?>
+                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        
+                        <?php
+                        
+                            if(!isset($_SESSION['user_id'])){
+                                echo "<li><a href='Login.php'>Login</a></li>";
+                                echo "<li><a href='AddUser.php'>Sign Up</a></li>";
+                            }else{
+                                echo "<li><a href='Logout.php'>Logout</a></li>";
+                            }
+                        
+                        ?>
+    
+                    </ul>
+                </div>
             </div>
-            <!--</div>-->
-            <!-- /.navbar-collapse -->
         </div>
