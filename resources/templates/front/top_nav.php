f2b0aec02d2349320abf2a55790430fca3c876a8
David Coe <coexx037@gmail.com>
2017-07-22 22:42:28 +0000

    change navbar

--- resources/templates/front/top_nav.php
+++ resources/templates/front/top_nav.php
@@ -12,7 +12,7 @@
                 <a class="navbar-brand" href="index.php">Home</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="navbar-header" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     
                     <?php
